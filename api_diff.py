90d588adc6938f09e1cb8acbf7f0f3a1b9376cca
Jonas Larsen <jonas@faglaback.net>
2026-01-13 19:37:44 +0100

    Ctrl+break now gracefully exits the main for-loop and saves the results to the Excel file

--- api_diff.py
+++ api_diff.py
@@ -118,44 +118,47 @@ def main() -> None:
 
         row_count = 0
         diff_count = 0
-        for row in test_cases:
-            api_params: dict[str, Any] = {
-                p["request_param"]: row[p["csv_column"]]
-                for p in config["param_mapping"]
-            }
-            old_method = config["old_api"].get("request_method", "GET")
-            new_method = config["new_api"].get("request_method", "GET")
-            old: dict[str, Any] = fetch(
-                config["old_api"]["url"],
-                method=old_method,
-                params=api_params,
-                headers=config["old_api"]["headers"]
-            )
-            new: dict[str, Any] = fetch(
-                config["new_api"]["url"],
-                method=new_method,
-                params=api_params,
-                headers=config["new_api"]["headers"]
-            )
-            logger.debug("Old: %s", old)
-            logger.debug("New: %s", new)
-            diff = DeepDiff(old, new, ignore_order=True)
-            result: dict[str, Any] = {
-                p["csv_column"]: row[p["csv_column"]] for p in config["param_mapping"]
-            }
-            result["has_diff"] = bool(diff)
-            result["has_data"] = not is_empty_json(old) or not is_empty_json(new)
-            result["diff"] = str(diff) if diff else ""
-            results.append(result)
-            if diff:
-                diff_count += 1
-                logger.info("%s: %s. %d diffs found. Row %d of %d", "-".join(str(row[p["csv_column"]]) for p in config["param_mapping"]), diff, diff_count, row_count, rows_to_run)
-            else:
-                logger.info("%s: No diff, has_data=%s. %d diffs found. Row %d of %d", "-".join(str(row[p["csv_column"]]) for p in config["param_mapping"]), result["has_data"], diff_count, row_count, rows_to_run)
-            if row_count % 1000 == 0:
-                api_diff_helpers.save_to_excel(results, args.output)
-                logger.info("Saved intermediate results. %d diffs found. Row %d of %d", diff_count, row_count, rows_to_run)
-            row_count += 1
+        try:
+            for row in test_cases:
+                api_params: dict[str, Any] = {
+                    p["request_param"]: row[p["csv_column"]]
+                    for p in config["param_mapping"]
+                }
+                old_method = config["old_api"].get("request_method", "GET")
+                new_method = config["new_api"].get("request_method", "GET")
+                old: dict[str, Any] = fetch(
+                    config["old_api"]["url"],
+                    method=old_method,
+                    params=api_params,
+                    headers=config["old_api"]["headers"]
+                )
+                new: dict[str, Any] = fetch(
+                    config["new_api"]["url"],
+                    method=new_method,
+                    params=api_params,
+                    headers=config["new_api"]["headers"]
+                )
+                logger.debug("Old: %s", old)
+                logger.debug("New: %s", new)
+                diff = DeepDiff(old, new, ignore_order=True)
+                result: dict[str, Any] = {
+                    p["csv_column"]: row[p["csv_column"]] for p in config["param_mapping"]
+                }
+                result["has_diff"] = bool(diff)
+                result["has_data"] = not is_empty_json(old) or not is_empty_json(new)
+                result["diff"] = str(diff) if diff else ""
+                results.append(result)
+                if diff:
+                    diff_count += 1
+                    logger.info("%s: %s. %d diffs found. Row %d of %d", "-".join(str(row[p["csv_column"]]) for p in config["param_mapping"]), diff, diff_count, row_count, rows_to_run)
+                else:
+                    logger.info("%s: No diff, has_data=%s. %d diffs found. Row %d of %d", "-".join(str(row[p["csv_column"]]) for p in config["param_mapping"]), result["has_data"], diff_count, row_count, rows_to_run)
+                if row_count % 1000 == 0:
+                    api_diff_helpers.save_to_excel(results, args.output)
+                    logger.info("Saved intermediate results. %d diffs found. Row %d of %d", diff_count, row_count, rows_to_run)
+                row_count += 1
+        except KeyboardInterrupt:
+            logger.info("Program interrupted by user.")
     else:
         raise ValueError("csv_file was not found in the config file")
 
